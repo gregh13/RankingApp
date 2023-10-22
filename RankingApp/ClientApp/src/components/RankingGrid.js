765df560693da8b80e0ec0a689dbed7e8c5b02c2
Gregory Herringer <greg.herringer@gmail.com>
2023-10-22 19:42:20 -0400

    implement grid creation functions (rows and cells)

--- RankingApp/ClientApp/src/components/RankingGrid.js
+++ RankingApp/ClientApp/src/components/RankingGrid.js
@@ -7,6 +7,23 @@ const RankingGrid = ({ items, imgArr }) => {
     const cellCollectionBottom = [];
 
 
+    function createCellsforRows() {
+
+    }
+
+    function createRowsForGrid() {
+        rankingGrid.push(<div className="rank-row top-tier">{cellCollectionTop}</div>)
+        rankingGrid.push(<div className="rank-row middle1-tier">{cellCollectionMiddle1}</div>)
+        rankingGrid.push(<div className="rank-row middle2-tier">{cellCollectionMiddle2}</div>)
+        rankingGrid.push(<div className="rank-row bottom-tier">{cellCollectionBottom}</div>)
+
+        return rankingGrid;
+    }
+
+    function createRankingGrid() {
+        createCellsForRows();
+        return createRowsForGrid();
+    }
 
     return (
 
