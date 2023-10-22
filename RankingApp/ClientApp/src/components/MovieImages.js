cb2ff390ffff9553ec2dcb376ac94c67b9bcb5ec
Gregory Herringer <greg.herringer@gmail.com>
2023-10-22 18:22:08 -0400

    create image array

--- RankingApp/ClientApp/src/components/MovieImages.js
+++ RankingApp/ClientApp/src/components/MovieImages.js
@@ -9,3 +9,17 @@ import SilentHill from '../image/movies/silent_hill.png';
 import TheRing from '../image/movies/the_ring.png';
 import ResidentEvil from '../image/movies/resident_evil.png';
 
+const MovieImageArr = [
+    { id: 1, image: Shining },
+    { id: 2, image: Exorcist },
+    { id: 3, image: HillsHaveEyes },
+    { id: 4, image: Hereditary },
+    { id: 5, image: Terrifier },
+    { id: 6, image: Saw },
+    { id: 7, image: Aliens },
+    { id: 8, image: SilentHill },
+    { id: 9, image: TheRing },
+    { id: 10, image: ResidentEvil },
+];
+
+export default MovieImageArr
