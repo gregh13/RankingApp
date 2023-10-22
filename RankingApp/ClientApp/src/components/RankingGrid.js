aecf1b2b08a10d774f0268d60cd2f9824a25c2d7
Gregory Herringer <greg.herringer@gmail.com>
2023-10-22 19:07:18 -0400

    initialize RankingGrid script

---
+++ RankingApp/ClientApp/src/components/RankingGrid.js
@@ -0,0 +1,19 @@
+﻿
+const RankingGrid = ({ items, imgArr }) => {
+    const rankingGrid = [];
+    const cellCollectionTop = [];
+    const cellCollectionMiddle1 = [];
+    const cellCollectionMiddle2 = [];
+    const cellCollectionBottom = [];
+
+
+
+    return (
+
+        <div className="rankings">
+            {createRankingGrid()}
+        </div>
+    )
+
+}
+export default RankingGrid;
