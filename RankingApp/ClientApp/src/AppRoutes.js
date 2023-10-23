4495444b38237f2c58e3b8e3f1b7ae67da1ebce1
Gregory Herringer <greg.herringer@gmail.com>
2023-10-23 19:40:03 -0400

    remove unneccesary items from navmenu and start page, add link for additional scary movies

--- RankingApp/ClientApp/src/AppRoutes.js
+++ RankingApp/ClientApp/src/AppRoutes.js
@@ -1,24 +1,16 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
+// import { Home } from "./components/Home";
 import RankItems from "./components/RankItems";
 
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <RankItems />
   },
   {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  },
-  {
-      path: '/rank-items',
-      element: <RankItems />
+      path: '/scary-movies',
+      element: <a href="https://www.imdb.com/search/title/?title_type=feature&genres=horror&sort=num_votes,desc"> IMDB Scary Movies</a> 
   }
 ];
 
--- RankingApp/ClientApp/src/components/Home.js
+++ RankingApp/ClientApp/src/components/Home.js
@@ -1,26 +1,53 @@
-import React, { Component } from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import React, { useState, useEffect } from 'react';
+import MovieImageArr from "./MovieImages.js";
+import RankingGrid from "./RankingGrid.js";
+import ItemCollection from "./ItemCollection";
 
-export class Home extends Component {
-  static displayName = Home.name;
+const RankItems = () => {
 
-  render() {
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+                { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+    }
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+
+    }, [])
+
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+        <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} drag={drag} allowDrop={allowDrop} drop={drop} />
+            <ItemCollection items={items} drag={drag} imgArr={MovieImageArr} />
+        </main>
+    )
 }
+export default RankItems;
